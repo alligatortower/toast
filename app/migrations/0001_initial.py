bb707d5d6d23c713d920b953a6e6742d3c1bec9e
Chris Weed <chris@lightmatter.com>
2015-06-14 10:12:01 -0400

    resetting migrations

--- app/migrations/0001_initial.py
+++ app/migrations/0001_initial.py
@@ -15,17 +15,18 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Drink',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
                 ('poisoned', models.BooleanField(default=False)),
             ],
         ),
         migrations.CreateModel(
             name='Game',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
-                ('created', model_utils.fields.AutoCreatedField(verbose_name='created', default=django.utils.timezone.now, editable=False)),
-                ('modified', model_utils.fields.AutoLastModifiedField(verbose_name='modified', default=django.utils.timezone.now, editable=False)),
-                ('gamestate', models.CharField(default='unstarted', choices=[('unstarted', 'Unstarted'), ('choosing', 'Waiting for Ambassador to choose server'), ('serving', 'Waiting for server to serve drinks'), ('trading', 'Waiting for Ambassador to propose toast'), ('ended', 'Ended')], max_length=200)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('created', model_utils.fields.AutoCreatedField(verbose_name='created', editable=False, default=django.utils.timezone.now)),
+                ('modified', model_utils.fields.AutoLastModifiedField(verbose_name='modified', editable=False, default=django.utils.timezone.now)),
+                ('gamestate', models.CharField(max_length=200, choices=[('unstarted', 'Unstarted'), ('choosing', 'Waiting for Ambassador to choose server'), ('serving', 'Waiting for server to serve drinks'), ('trading', 'Waiting for Ambassador to propose toast'), ('ended', 'Ended')], default='unstarted')),
+                ('winners', models.CharField(blank=True, choices=[('loyalist', 'Loyalist'), ('traitor', 'Traitor')], null=True, max_length=200)),
             ],
             options={
                 'abstract': False,
@@ -34,15 +35,17 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Player',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
-                ('team', models.CharField(default='unassigned', choices=[('unassigned', 'Unassigned'), ('ambassador', 'Ambassador'), ('loyalist', 'Loyalist'), ('traitor', 'Traitor'), ('dead', 'Dead')], max_length=20)),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, primary_key=True, serialize=False)),
+                ('team', models.CharField(max_length=20, choices=[('loyalist', 'Loyalist'), ('traitor', 'Traitor'), ('unassigned', 'Unassigned'), ('ambassador', 'Ambassador'), ('dead', 'Dead')], default='unassigned')),
+                ('name', models.CharField(blank=True, null=True, max_length=200)),
                 ('session_key', models.CharField(max_length=200)),
                 ('alive', models.BooleanField(default=True)),
                 ('has_poison', models.BooleanField(default=False)),
                 ('server', models.BooleanField(default=False)),
                 ('game_owner', models.BooleanField(default=False)),
+                ('last_trade', models.CharField(blank=True, null=True, max_length=200)),
                 ('game', models.ForeignKey(to='app.Game')),
-                ('wants_to_trade', models.ManyToManyField(null=True, related_name='wants_to_trade_with_me', blank=True, to='app.Player')),
+                ('wants_to_trade', models.ManyToManyField(related_name='wants_to_trade_with_me', blank=True, null=True, to='app.Player')),
             ],
         ),
         migrations.AddField(
